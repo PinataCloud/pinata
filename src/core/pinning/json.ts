7c708ca5b5b2dd776222e0af9c81a829649db6f6
Steve <stevedsimkins@gmail.com>
2024-07-17 11:51:00 -0400

    updates to json uploads and types

--- src/core/pinning/json.ts
+++ src/core/pinning/json.ts
@@ -3,47 +3,51 @@
  * @returns message
  */
 
-import type { PinataConfig, PinResponse, UploadOptions } from "../types";
+import type {
+  PinataConfig,
+  PinResponse,
+  UploadOptions,
+  JsonBody,
+} from "../types";
 
-export const uploadJson = async (
-	config: PinataConfig | undefined,
-	jsonData: any,
-	options?: UploadOptions,
+export const uploadJson = async <T extends JsonBody>(
+  config: PinataConfig | undefined,
+  jsonData: T,
+  options?: UploadOptions,
 ) => {
-	try {
-		let jwt;
-		if (options && options.keys) {
-			jwt = options.keys;
-		} else {
-			jwt = config?.pinataJwt;
-		}
+  let jwt: string | undefined;
+  if (options?.keys) {
+    jwt = options.keys;
+  } else {
+    jwt = config?.pinataJwt;
+  }
 
-		const data = JSON.stringify({
-			pinataContent: jsonData,
-			pinataOptions: {
-				cidVersion: options?.cidVersion,
-				groupId: options?.groupId,
-			},
-			pinataMetadata: {
-				name: options?.metadata ? options.metadata.name : "json",
-				keyvalues: options?.metadata?.keyValues,
-			},
-		});
+  const data = JSON.stringify({
+    pinataContent: jsonData,
+    pinataOptions: {
+      cidVersion: options?.cidVersion,
+      groupId: options?.groupId,
+    },
+    pinataMetadata: {
+      name: options?.metadata ? options.metadata.name : "json",
+      keyvalues: options?.metadata?.keyValues,
+    },
+  });
 
-		const request = await fetch(
-			`https://api.pinata.cloud/pinning/pinJSONToIPFS`,
-			{
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-					Authorization: `Bearer ${jwt}`,
-				},
-				body: data,
-			},
-		);
-		const res: PinResponse = await request.json();
-		return res;
-	} catch (error) {
-		throw error;
-	}
+  const request = await fetch(
+    "https://api.pinata.cloud/pinning/pinJSONToIPFS",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwt}`,
+      },
+      body: data,
+    },
+  );
+  if (!request.ok) {
+    throw new Error("Problem uploading JSON");
+  }
+  const res: PinResponse = await request.json();
+  return res;
 };
