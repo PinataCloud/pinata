214d42cf6840f8000378f1046cb312da38a18de1
Steve <stevedsimkins@gmail.com>
2024-07-22 10:33:59 -0400

    updated revoke keys

--- src/core/keys/revokeKeys.ts
+++ src/core/keys/revokeKeys.ts
@@ -6,45 +6,41 @@
 import type { PinataConfig, RevokeKeyResponse } from "../types";
 
 const wait = (milliseconds: number) => {
-	return new Promise((resolve) => {
-		setTimeout(resolve, milliseconds);
-	});
+  return new Promise((resolve) => {
+    setTimeout(resolve, milliseconds);
+  });
 };
 
 export const revokeKeys = async (
-	config: PinataConfig | undefined,
-	keys: string[],
-) => {
-	try {
-		const responses: RevokeKeyResponse[] = [];
-		for (const key of keys) {
-			const request = await fetch(
-				`https://api.pinata.cloud/v3/pinata/keys/${key}`,
-				{
-					method: "PUT",
-					headers: {
-						"Content-Type": "application/json",
-						Authorization: `Bearer ${config?.pinataJwt}`,
-					},
-				},
-			);
-			await wait(300);
-			if (!request.ok) {
-				const res: any = await request.json();
-				responses.push({
-					key: key,
-					status: res.error.message,
-				});
-			} else {
-				const res: string = await request.json();
-				responses.push({
-					key: key,
-					status: res,
-				});
-			}
-		}
-		return responses;
-	} catch (error: any) {
-		throw error.message;
-	}
+  config: PinataConfig | undefined,
+  keys: string[],
+): Promise<RevokeKeyResponse[]> => {
+  const responses: RevokeKeyResponse[] = [];
+  for (const key of keys) {
+    const request = await fetch(
+      `https://api.pinata.cloud/v3/pinata/keys/${key}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${config?.pinataJwt}`,
+        },
+      },
+    );
+    await wait(300);
+    if (!request.ok) {
+      const res: any = await request.json();
+      responses.push({
+        key: key,
+        status: res.error.message,
+      });
+    } else {
+      const res: string = await request.json();
+      responses.push({
+        key: key,
+        status: res,
+      });
+    }
+  }
+  return responses;
 };
