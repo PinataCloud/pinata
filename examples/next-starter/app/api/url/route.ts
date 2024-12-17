bfc218a91b12798957fd31d9393128b396e1e90e
Steve <stevedsimkins@gmail.com>
2024-12-17 11:17:51 -0500

    chore: Refacted next app to use signed urls for uploads

---
+++ examples/next-starter/app/api/url/route.ts
@@ -0,0 +1,19 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { pinata } from "@/utils/config";
+
+export const dynamic = "force-dynamic";
+
+export async function GET(req: NextRequest, res: NextResponse) {
+	try {
+		const url = await pinata.upload.createSignedURL({
+			expires: 30,
+		});
+		return NextResponse.json({ url: url }, { status: 200 });
+	} catch (error) {
+		console.log(error);
+		return NextResponse.json(
+			{ text: "Error creating API Key:" },
+			{ status: 500 },
+		);
+	}
+}
