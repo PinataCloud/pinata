8d6b5edbaa0605dd6ad3554b505af52000e510f3
Steve <stevedsimkins@gmail.com>
2025-02-19 13:48:26 -0500

    chore: Updated next starter

--- examples/next-starter/app/api/sign/route.ts
+++ examples/next-starter/app/api/sign/route.ts
@@ -4,18 +4,19 @@ import { pinata } from "@/utils/config";
 export const dynamic = "force-dynamic";
 
 export async function POST(req: NextRequest, res: NextResponse) {
-	try {
-		const data = await req.json();
-		const url = await pinata.gateways.createSignedURL({
-			cid: data.cid,
-			expires: 30,
-		});
-		return NextResponse.json(url, { status: 200 });
-	} catch (error) {
-		console.log(error);
-		return NextResponse.json(
-			{ text: "Error creating API Key:" },
-			{ status: 500 },
-		);
-	}
+  try {
+    const data = await req.json();
+    console.log(pinata.config?.pinataGateway)
+    const url = await pinata.gateways.private.createAccessLink({
+      cid: data.cid,
+      expires: 30,
+    });
+    return NextResponse.json(url, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { text: "Error creating API Key:" },
+      { status: 500 },
+    );
+  }
 }
--- examples/next-starter/utils/config.ts
+++ examples/next-starter/utils/config.ts
@@ -1,8 +1,9 @@
-"server only";
 
-import { PinataSDK } from "../../../dist";
+import { PinataSDK } from "../../../dist"
 
 export const pinata = new PinataSDK({
-  pinataJwt: `${process.env.PINATA_JWT}`,
-  pinataGateway: `${process.env.NEXT_PUBLIC_GATEWAY_URL}`,
-});
+  pinataJwt: `${process.env.DEV_PINATA_JWT}`,
+  pinataGateway: `${process.env.NEXT_PUBLIC_DEV_GATEWAY_URL}`,
+  uploadUrl: `${process.env.DEV_UPLOAD_URL}`,
+  endpointUrl: `${process.env.DEV_ENDPOINT_URL}`
+})
