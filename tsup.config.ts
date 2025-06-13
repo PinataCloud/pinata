cbc86b8689977e43de88b36f3abee30c0a364992
Steve <stevedsimkins@gmail.com>
2025-06-13 09:26:55 -0400

    feat: Merge expo package into main Pinata SDK
    This moves the pinata-expo-hooks package into the main Pinata SDK. By
    separating the package imports it should prevent the need to install
    expo or react deps when not in use

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,19 +1,25 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/react/index.ts"],
-  format: ["cjs", "esm"], // Build for commonJS and ESmodules
-  dts: true, // Generate declaration file (.d.ts)
-  splitting: true,
-  sourcemap: true,
-  clean: true,
-  external: ["react", "react-dom"],
-  outDir: "dist",
-  treeshake: true,
-  outExtension({ format }) {
-    return {
-      js: format === "cjs" ? ".js" : ".mjs",
-    };
-  },
-
+	entry: ["src/index.ts", "src/react/index.ts", "src/expo/index.ts"],
+	format: ["cjs", "esm"], // Build for commonJS and ESmodules
+	dts: true, // Generate declaration file (.d.ts)
+	splitting: true,
+	sourcemap: true,
+	clean: true,
+	external: [
+		"react",
+		"react-dom",
+		"react-native",
+		"expo",
+		"expo-file-system",
+		"Base64",
+	],
+	outDir: "dist",
+	treeshake: true,
+	outExtension({ format }) {
+		return {
+			js: format === "cjs" ? ".js" : ".mjs",
+		};
+	},
 });
